519975322ed7de43fe9c0737d99ea802216e86e4
dvasunin <dmitrii.vasiunin@telekom.com>
2024-02-08 12:36:20 +0200

    add converter test

--- src/test/java/org/eclipse/tractusx/selfdescriptionfactory/SelfDescriptionFactoryApplicationTests.java
+++ src/test/java/org/eclipse/tractusx/selfdescriptionfactory/SelfDescriptionFactoryApplicationTests.java
@@ -1,6 +1,6 @@
 /********************************************************************************
- * Copyright (c) 2022,2023 T-Systems International GmbH
- * Copyright (c) 2022,2023 Contributors to the Eclipse Foundation
+ * Copyright (c) 2022,2024 T-Systems International GmbH
+ * Copyright (c) 2022,2024 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
@@ -20,15 +20,82 @@
 
 package org.eclipse.tractusx.selfdescriptionfactory;
 
-import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import jakarta.annotation.PostConstruct;
+import org.eclipse.tractusx.selfdescriptionfactory.model.vrel3.SelfdescriptionPostRequest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.convert.ConversionService;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SelfDescriptionFactoryApplicationTests {
 
-    @Test
-    void contextLoads() {
-        // here we assert the context is brought up
+    @Autowired
+    ConversionService conversionService;
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @PostConstruct
+    public void init() {
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
     }
 
+
+    static final String legalPersonStr =
+            """
+                    {
+                      "externalId": "ID01234-123-4321",
+                      "type": "LegalParticipant",
+                      "holder": "BPNL000000000000",
+                      "issuer": "CAXSDUMMYCATENAZZ",
+                      "registrationNumber": [
+                        {
+                          "type": "taxID",
+                          "value": "o12345678"
+                        }
+                      ],
+                      "headquarterAddress.country": "DE",
+                      "legalAddress.country": "DE",
+                      "bpn": "BPNL000000000000"
+                    }
+                    """;
+    static final String serviceOfferingStr =
+            """
+                    {
+                      "externalId": "ID01234-123-4321",
+                      "type": "ServiceOffering",
+                      "holder": "BPNL000000000000",
+                      "issuer": "CAXSDUMMYCATENAZZ",
+                      "providedBy": "https://participant.url",
+                      "aggregationOf": "https://aggr1.url, https://aggr2.url",
+                      "termsAndConditions": "https://raw.githubusercontent.com/eclipse-tractusx/sd-factory/main/LICENSE",
+                      "policies": "policy1, policy2"
+                    }
+                    """;
+
+    static Stream<Arguments> inputProvider() {
+        return Stream.of(
+                Arguments.of(legalPersonStr, "Legal Participant"),
+                Arguments.of(serviceOfferingStr, "Service Offering")
+        );
+    }
+
+    @ParameterizedTest(name = "{index} - {1}")
+    @MethodSource("inputProvider")
+    public void testLegalPersonConverter(String selfdescriptionRequestStr, String type) throws JsonProcessingException {
+        var selfdescriptionPostRequest = objectMapper.readValue(selfdescriptionRequestStr, SelfdescriptionPostRequest.class);
+        var converted = conversionService.convert(selfdescriptionPostRequest, SelfDescription.class);
+        assertNotNull(converted, "converted object should not be null");
+        assertFalse(converted.getVerifiableCredentialList().isEmpty());
+        System.out.println(objectMapper.writeValueAsString(converted.getVerifiableCredentialList()));
+    }
 }
